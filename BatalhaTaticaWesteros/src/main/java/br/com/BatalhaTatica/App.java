82c706337c340be0154da702810fab719dd6bb0d
João Vitor Almeida <joaovitoraq2006@gmail.com>
2025-10-27 10:26:21 -0300

    Implementa a logica para gerar e criar os personagens robos

--- BatalhaTaticaWesteros/src/main/java/br/com/BatalhaTatica/App.java
+++ BatalhaTaticaWesteros/src/main/java/br/com/BatalhaTatica/App.java
@@ -1,9 +1,9 @@
 package br.com.BatalhaTatica;
 
-import br.com.BatalhaTatica.model.*;
+import br.com.BatalhaTatica.model.Tabuleiro;
 import br.com.BatalhaTatica.service.Jogo;
-import br.com.BatalhaTatica.service.Movimentacao;
 import br.com.BatalhaTatica.view.JogoVisualizer;
+
 import java.util.Scanner;
 
 public class App {
@@ -15,21 +15,6 @@ public class App {
         JogoVisualizer jogoVisualizer = new JogoVisualizer(tabuleiro);
         Jogo jogo = new Jogo();
 
-        Personagem player1 = jogo.criarPersonagem(sc, 1);
-        Personagem stark1 = new Stark(101, "Tony");
-        Personagem lanni = new Lannister(2, "Lol");
-        Personagem targ = new Targaryen(3, "Targ");
-
-        Movimentacao movimentacao1 = new Movimentacao(tabuleiro);
-
-        movimentacao1.setPosicaoInicial(stark1);
-        movimentacao1.setPosicaoInicial(lanni);
-        movimentacao1.setPosicaoInicial(targ);
-        movimentacao1.setPosicaoInicial(player1);
-
-        System.out.println(JogoVisualizer.gerarTabelaCasas());
-        String tabuleiroFormatado = jogoVisualizer.imprimeTabuleiro();
-        System.out.println(tabuleiroFormatado);
 
     }
 }
--- BatalhaTaticaWesteros/src/main/java/br/com/BatalhaTatica/service/Bots.java
+++ BatalhaTaticaWesteros/src/main/java/br/com/BatalhaTatica/service/Bots.java
@@ -1,4 +1,64 @@
 package br.com.BatalhaTatica.service;
 
+import br.com.BatalhaTatica.model.Casas;
+
+import java.util.Random;
+
 public class Bots {
+
+    private Jogo jogo;
+    private Random random = new Random();
+
+    public Bots(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public int geraNumero() {
+        int min = 1;
+        int max = 3;
+        int numAleatorio = min + random.nextInt(max - min + 1);
+        return numAleatorio;
+    }
+
+    public Casas geraCasa(int numeroGerado) {
+        switch (numeroGerado) {
+            case 1:
+                return Casas.STARK;
+            case 2:
+                return Casas.TARGARYEN;
+            case 3:
+                return Casas.LANNISTER;
+            default:
+                return null;
+        }
+    }
+
+    public String geraNomeCompleto(String numBotGerado) {
+        return "Bot" + numBotGerado;
+    }
+
+    public void criarBots() {
+        int qtdStark = 0;
+        int qtdLannister = 0;
+        int qtdTargaryen = 0;
+
+        for (int i = 1; i <= 3; i++) {
+            int numeroGerado = this.geraNumero();
+            Casas casaGerada = this.geraCasa(numeroGerado);
+
+            String numBot;
+            if (casaGerada == Casas.LANNISTER)
+                numBot = "Lannister" + String.valueOf(++qtdLannister);
+            else if (casaGerada == Casas.STARK)
+                numBot = "Stark" + String.valueOf(++qtdStark);
+            else
+                numBot = "Targaryen" + String.valueOf(++qtdTargaryen);
+
+            String nome = this.geraNomeCompleto(numBot);
+
+            this.jogo.criarPersonagem(i, nome, casaGerada);
+        }
+    }
+
+
 }
